c60607152da0365c698789393b829844adb0d657
santosh <santoshpaudyal99@gmail.com>
2020-03-09 20:19:15 +0545

    testing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 const User = require("./models/user");
 
 const MONGODB_URI =
-  "mongodb+srv://<Username>:<Password>@cluster0-q3o0u.mongodb.net/shop?retryWrites=true&w=majority";
+  // "mongodb+srv://<Username>:<Password>@cluster0-q3o0u.mongodb.net/shop?retryWrites=true&w=majority";
 
 const app = express();
 const store = new MongoDbStore({
